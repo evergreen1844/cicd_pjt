3f8cd23d0c146825239cad0b3545ff4b37998ce0
root <djlee@itcen.com>
2026-02-02 17:37:57 +0900

    test

--- app/src/main/java/com/example/demo/controller/HomeController.java
+++ app/src/main/java/com/example/demo/controller/HomeController.java
@@ -6,19 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";   // login.html
-    }
-
-    @GetMapping("/home")
-    public String home() {
-        return "home";    // home.html
-    }
-
     @GetMapping("/")
-    public String root() {
-        return "redirect:/login";
+    public String home() {
+        return "home";
     }
 }
 
