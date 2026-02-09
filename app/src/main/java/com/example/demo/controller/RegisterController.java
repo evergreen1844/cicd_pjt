46d5577a1706abc225fb25fee37a7cb89fa7717c
root <djlee@itcen.com>
2026-02-09 15:14:02 +0900

    test

--- app/src/main/java/com/example/demo/controller/RegisterController.java
+++ app/src/main/java/com/example/demo/controller/RegisterController.java
@@ -1,15 +1,17 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequiredArgsConstructor
 public class RegisterController {
 
     private final UserService service;
+
+    public RegisterController(UserService service) {
+        this.service = service;
+    }
 
     @GetMapping("/register")
     public String registerPage() {
--- app/src/main/java/com/example/demo/service/UserService.java
+++ app/src/main/java/com/example/demo/service/UserService.java
@@ -2,23 +2,26 @@ package com.example.demo.service;
 
 import com.example.demo.domain.User;
 import com.example.demo.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository repo;
     private final PasswordEncoder encoder;
+
+    public UserService(UserRepository repo, PasswordEncoder encoder) {
+        this.repo = repo;
+        this.encoder = encoder;
+    }
 
     public void register(String username, String password) {
 
         User user = new User();
         user.setUsername(username);
 
-        // ν ½ν΄¥ μ•”νΈν™”
+        // encode password
         user.setPassword(encoder.encode(password));
 
         user.setRole("USER");
